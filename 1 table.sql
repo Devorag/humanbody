8413be54f2315dafb9ace9523f8477a053285d3c
Devorag <141979085+Devorag@users.noreply.github.com>
2023-11-27 17:20:10 -0500

    added sample data

--- 1 table.sql
+++ 1 table.sql
@@ -1,3 +1,13 @@
 use humanbodyDB
 go 
-create table dbo.body(id int not null identity primary key)
+create table dbo.body(id int not null identity primary key,
+bodypart varchar(100) not null,
+num int not null
+)
+
+insert body(bodypart, num)
+select 'eyes', 2
+union select 'ears', 2
+union select 'nose', 1
+union select 'mouth', 2
+union select 'teeth', 32
