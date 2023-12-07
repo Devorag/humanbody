693b650139aa7cfbee963ade17585e7bb87746db
Devorag <141979085+Devorag@users.noreply.github.com>
2023-12-06 22:04:22 -0500

    made the num column nullable

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,8 +1,10 @@
 use humanbodyDB
 go 
+drop table if exists body 
+go 
 create table dbo.body(id int not null identity primary key,
 bodypart varchar(100) not null,
-num int not null
+num int null
 )
 
 insert body(bodypart, num)
